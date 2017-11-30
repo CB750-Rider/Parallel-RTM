b1391f92b8933c4401d054b24b814cd101e810dd
Lee Burchett <lee.r.burchett@gmail.com>
2017-11-29 19:20:42 -0500

    Update SRSC.c

--- SRSC.c
+++ SRSC.c
@@ -1,4 +1,4 @@
-/*
+/* Serial Radiative Spectrum Calculator
  * SRSC.c
  *
  *  Created on: Oct 26, 2017
@@ -8,7 +8,25 @@
  *      implementation of the banded matrix
  *      calculation of refractive index spectra
  *      from the HITRAN database
- *
+ *    
+    Copyright (C) 2017 Dr. Lee R. Burchett
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+For more information contact Dr. Lee R. Burchett at 
+lee.r.burchett@gmail.com
+
  */
 
 #include "SRSC.h"
